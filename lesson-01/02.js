34bbbec08a72177185c0cca2f0b44f2dc12e1222
Nadezhda Sanfirova <exo76@yandex.ru>
2023-07-26 13:42:09 +0300

    Added text

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,11 +1,11 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-console.log(number1)
+console.log('Вставь сюда полученное число:'+' '+number1);
 
  {
     if (number1 % 2 === 0) {
-      console.log ('Even');
+      console.log ('Это четное число.');
     } else {
-      console.log ('Odd');
+      console.log ('Это нечетное число.');
     }
   };
