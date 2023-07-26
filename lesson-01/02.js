4daa7f21860d7d1e1cf76b9ea32da8f5ab3464aa
Nadezhda Sanfirova <exo76@yandex.ru>
2023-07-26 13:30:21 +0300

    Task 2 Lesson 1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,11 @@
 // code
+let number1 = Math.floor(Math.random() * 100);
+console.log(number1)
+
+ {
+    if (number1 % 2 === 0) {
+      console.log ('Even');
+    } else {
+      console.log ('Odd');
+    }
+  };
