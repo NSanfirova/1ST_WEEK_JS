81e1e8e5c96b8676f2cc7c1c15aa67ccfd2dbd90
Nadezhda Sanfirova <exo76@yandex.ru>
2023-07-26 14:20:29 +0300

    Remove a string

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,11 +1,9 @@
 
 let number1 = Math.floor(Math.random() * 100);
-console.log('Вставь сюда полученное число:'+' '+number1);
-
  
     if (number1 % 2 === 0) {
-      console.log ('Это четное число.');
+      console.log ('Вставь сюда полученное число: '+ number1 + ' Это четное число.');
     } else {
-      console.log ('Это нечетное число.');
+      console.log ('Вставь сюда полученное число: '+ number1 + ' Это нечетное число.');
     }
- ;
+ 
