c4e065dbc69ed2cc2503360c48970b587372a47c
Nadezhda Sanfirova <exo76@yandex.ru>
2023-07-26 15:13:22 +0300

    add empty spaces

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,6 @@
 let number1 = Math.floor(Math.random() * 100);
-if (number1%2===0){
-  console.log(number1+":Это четное число.");
+if (number1 % 2===0){
+  console.log(number1+'четное число');
 } else {
-  console.log(number1+":Это нечетное число.");
+  console.log(number1+'нечетное число');
 }
